c326f9df9a6c901a5074cdaf3c39c995f68f6295
Hasham Vakani <hashamvakani@users.noreply.github.com>
2020-08-30 04:30:50 +0500

    Finalization

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
 
 function Home() {
   return (
     <div>
-      <h1>Welcome to Home</h1>
+      <React.Fragment>
+          <CssBaseline />
+              <h2 className="head-one">Online Order & Store Opening/Safety Info</h2>
+          <Container maxWidth="lg" className="container1">
+            <div>
+              <h1 className="Header">"HV Shoe Store"</h1>
+              <h2 className="Header-two">SHOE CARNIVAL ONLINE SHOE STORE: SHOP SALES ON SHOES FOR THE ENTIRE FAMILY</h2>
+          </div>
+      </Container>
+              <img src="https://source.unsplash.com/weekly?shoe" alt="Shoe Image" className="Shoe-img"/>
+    </React.Fragment>
     </div>
   );
 }
--- src/Components/ProductItems.js
+++ src/Components/ProductItems.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useParams } from 'react-router-dom'
 import Shoes from './../shoes.json'
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
 
 
 
@@ -11,16 +14,34 @@ function ProductItem() {
 
   if (!shoe)
     return <h2>Poduct not found</h2>
-  console.log(shoe)
+
   return (
     <div>
-      <h1>Welcome to ProductItem J.s</h1>
       <div>
-        <h2>{shoe.name}</h2>
-        <img src={shoe.img} height={150} alt={shoe.name} />
-      </div>
+        {/* <h2>{shoe.name}</h2>
+        <img src={shoe.img} height={150} alt={shoe.name} /> */}
+
+        <React.Fragment>
+          <CssBaseline />
+          <Container maxWidth="lg" className="container1">
+            <div>
+              <div>
+                <Paper variant="outlined" className='PaperHead1'>
+                  <div >
+                    <h2 className="Product-head1">{shoe.name}</h2>
+                    <img src={shoe.img} height={150} alt={shoe.name} className="Product-img1" />
+                    <hr />
+                      <h1 className="Pirce-tag-in">{shoe.price}</h1>
+                  </div>
+                </Paper>
+              </div>
+          </div>
+      </Container>
+    </React.Fragment>
     </div>
+    </div >
   );
 }
 
 export default ProductItem;
+
